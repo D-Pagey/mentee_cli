5734c9ac766971ebc5e4dcba08bef155e6d5df15
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-08-02 21:53:40 +0100

    remove old boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,15 @@ struct Args {
     /// Name of the mentee
     #[arg(short, long)]
     name: Option<String>,
-    // /// Number of times to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
 }
 
 fn main() {
     let args = Args::parse();
 
-    // for _ in 0..args.count {
-    // println!("Hello {:?}!", args.name);
-    // }
-
+    // TODO: if name arg then fetch that mentee
+    // else just render entire table
     match args.name {
         Some(name) => println!("Hello {}", name),
         None => println!("No name provided"),
     }
-    //
-    //
-    //
-    // if (args.name) {
-    //     println!("Hello {:?}!", args.name);
-    // } else {
-    //     println!("No name given");
-    // }
-    // TODO: if name arg then fetch that mentee
-    // else just render entire table
 }
