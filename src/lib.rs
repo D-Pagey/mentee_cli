f58f88a176bee00f973d2b05105f1671f97ea9d8
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-08-26 22:28:30 +0100

    create into its own method

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,8 @@ pub fn run(conn: Connection) -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Show => get_all_mentees(&conn).unwrap(),
-        Commands::Create => println!("Creating a new mentee"),
+        Commands::Show => get_all_mentees(&conn)?,
+        Commands::Create => create_mentee(&conn)?,
         Commands::Delete => println!("Deleting a mentee..."),
     }
 
@@ -41,6 +41,41 @@ struct Mentee {
 }
 
 fn get_all_mentees(conn: &Connection) -> Result<()> {
+    let mut stmt = conn.prepare("SELECT id, name, calls_per_month FROM mentee")?;
+    let mentee_iter = stmt.query_map([], |row| {
+        Ok(Mentee {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            calls_per_month: row.get(2)?,
+        })
+    })?;
+
+    let _table: Vec<Mentee> = Vec::new();
+
+    for mentee in mentee_iter {
+        println!("Found mentee {:?}", mentee.unwrap());
+        // table.push(mentee)
+    }
+
+    // let table = vec![
+    //     vec!["AA".cell(), "1 year".cell().justify(Justify::Right)],
+    //     vec!["MD".cell(), "6 months".cell().justify(Justify::Right)],
+    //     vec!["MS".cell(), "3 months".cell().justify(Justify::Right)],
+    //     vec!["PM".cell(), "5 months".cell().justify(Justify::Right)],
+    //     vec!["AL".cell(), "8 months".cell().justify(Justify::Right)],
+    //     vec!["DG".cell(), "4 months".cell().justify(Justify::Right)],
+    // ]
+    // .table()
+    // .title(vec![
+    //     "Initials".cell().bold(true),
+    //     "Duration".cell().bold(true),
+    // ])
+    // .bold(true);
+    // assert!(print_stdout(table).is_ok());
+    Ok(())
+}
+
+fn create_mentee(conn: &Connection) -> Result<()> {
     let alex = Mentee {
         id: 0,
         name: "alex".to_string(),
@@ -52,18 +87,7 @@ fn get_all_mentees(conn: &Connection) -> Result<()> {
         (&alex.name, &alex.calls_per_month),
     )?;
 
-    let mut stmt = conn.prepare("SELECT id, name, calls_per_month FROM mentee")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Mentee {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            calls_per_month: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
+    println!("mentee created");
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -21,39 +21,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-// println!("{:?}", args)
-
-// let table = vec![
-//     vec!["AA".cell(), "1 year".cell().justify(Justify::Right)],
-//     vec!["MD".cell(), "6 months".cell().justify(Justify::Right)],
-//     vec!["MS".cell(), "3 months".cell().justify(Justify::Right)],
-//     vec!["PM".cell(), "5 months".cell().justify(Justify::Right)],
-//     vec!["AL".cell(), "8 months".cell().justify(Justify::Right)],
-//     vec!["DG".cell(), "4 months".cell().justify(Justify::Right)],
-// ]
-// .table()
-// .title(vec![
-//     "Initials".cell().bold(true),
-//     "Duration".cell().bold(true),
-// ])
-// .bold(true);
-// assert!(print_stdout(table).is_ok());
-// TODO: if name arg then fetch that mentee
-// else just render entire table
-// match args.name {
-//     Some(name) => println!("Hello {}", name),
-//     None => print_stdout(table).expect("Error reading table"),
-// }
-//
-// use clitable::{format::Justify, print_stdout, Cell, Style, Table};
-
-// struct Mentee {
-//     id: i32,
-//     name: String,
-//     calls_per_month: i32,
-// }
-
-// Name of the mentee
-//     #[arg(short, long)]
-//     name: Option<String>,
