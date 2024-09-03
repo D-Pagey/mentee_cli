04af5406d522bd585fcfd492544696d4d6854e46
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-09-03 12:00:39 +0100

    handle updating calls

--- src/lib.rs
+++ src/lib.rs
@@ -143,6 +143,16 @@ fn delete_mentee(conn: &Connection, name: String) {
     }
 }
 
-fn update_mentee(_conn: &Connection, name: String) {
-    println!("Updating...{name}")
+fn update_mentee(conn: &Connection, name: String) {
+    let calls = Text::new("How many calls per month do they have?")
+        .prompt()
+        .expect("Failed to capture mentee name");
+
+    match conn.execute(
+        "UPDATE mentee SET calls = ?1 WHERE name = ?2",
+        (&calls, &name),
+    ) {
+        Ok(updated) => println!("updated...{updated}"),
+        Err(error) => println!("you fucked up - {error}"),
+    }
 }
