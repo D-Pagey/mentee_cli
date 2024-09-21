b0e73125534a841db24316d72b1da9e70be92dfb
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-09-21 23:09:13 +0100

    add utils tests

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,9 @@ use inquire::validator::Validation;
 use std::error::Error;
 
 pub fn validate_name_core(s: &str) -> Result<(), String> {
-    if s.chars().all(|c| c.is_alphabetic() || c.is_whitespace()) {
+    if s.trim().is_empty() {
+        Err("Name cannot be empty or just whitespace.".to_string())
+    } else if s.chars().all(|c| c.is_alphabetic() || c.is_whitespace()) {
         Ok(())
     } else {
         Err("Name can only contain letters and spaces.".to_string())
@@ -60,11 +62,55 @@ mod tests {
     }
 
     #[test]
+    fn error_for_empty_name() {
+        let result = validate_name_core("     ");
+        assert_eq!(
+            result,
+            Err(String::from("Name cannot be empty or just whitespace."))
+        );
+    }
+
+    #[test]
     fn error_for_invalid_name() {
         let result = validate_name_core("d#n");
         assert_eq!(
             result,
             Err(String::from("Name can only contain letters and spaces."))
         );
+    }
+
+    #[test]
+    fn inquire_valid_name() {
+        let result = inquire_validate_name("dan");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), Validation::Valid);
+    }
+
+    #[test]
+    fn inquire_invalid_name() {
+        let result = inquire_validate_name("d#n");
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "Name can only contain letters and spaces."
+        );
+    }
+
+    #[test]
+    fn clap_valid_name() {
+        let name = "dan";
+        let result = clap_validate_name(name);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), name)
+    }
+
+    #[test]
+    fn clap_invalid_name() {
+        let result = clap_validate_name("d#n");
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            "Name can only contain letters and spaces."
+        )
     }
 }
