1f69f213d6a63482dfde33f65276e785f04f9451
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-09-21 23:23:11 +0100

    all unit tests for utils

--- src/utils.rs
+++ src/utils.rs
@@ -25,8 +25,6 @@ pub fn clap_validate_name(s: &str) -> Result<String, String> {
     }
 }
 
-// Define a reusable validator for days of the month
-// fn validate_day_core(input: &u32) -> Result<Validation, Box<dyn Error + Send + Sync>> {
 fn validate_day_core(input: &u32) -> Result<(), String> {
     if *input >= 1 && *input <= 31 {
         Ok(())
@@ -111,6 +109,57 @@ mod tests {
         assert_eq!(
             result.unwrap_err(),
             "Name can only contain letters and spaces."
+        )
+    }
+
+    #[test]
+    fn valid_day() {
+        let result = validate_day_core(&2);
+        assert!(result.is_ok());
+        assert_eq!(result, Ok(()))
+    }
+
+    #[test]
+    fn invalid_day() {
+        let result = validate_day_core(&60);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "The day must be between 1 and 31."
+        )
+    }
+
+    #[test]
+    fn inquire_valid_day() {
+        let result = inquire_validate_day(&20);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), Validation::Valid)
+    }
+
+    #[test]
+    fn inquire_invalid_day() {
+        let result = inquire_validate_day(&70);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "The day must be between 1 and 31."
+        )
+    }
+
+    #[test]
+    fn clap_valid_day() {
+        let result = clap_validate_day("20");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "20")
+    }
+
+    #[test]
+    fn clap_invalid_day() {
+        let result = clap_validate_day("70");
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "The day must be between 1 and 31."
         )
     }
 }
